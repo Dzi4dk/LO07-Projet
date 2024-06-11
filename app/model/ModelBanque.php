2448e7f6d78751a27347341a08685183ed468bde
Dzi4dk <louis.2lhomme@gmail.com>
2024-06-11 16:58:06 +0200

    Banque : view all
    v1 non fonctionnelle

---
+++ app/model/ModelBanque.php
@@ -0,0 +1,207 @@
+
+<!-- ----- debut ModelVin -->
+
+<?php
+require_once 'Model.php';
+
+class ModelBanque {
+ private $id, $label, $pays;
+ 
+
+ // pas possible d'avoir 2 constructeurs
+ public function __construct($id = NULL, $label = NULL, $pays = NULL) {
+  // valeurs nulles si pas de passage de parametres
+  if (!is_null($id)) {
+   $this->id = $id;
+   $this->label = $label;
+   $this->pays = $pays;
+  }
+ }
+ 
+ 
+//Setters
+ 
+ function setLabel($label) {
+  $this->label = $label;
+ }
+
+ function setPays($pays) {
+  $this->pays = $pays;
+ }
+ 
+ function setId($id) {
+  $this->id = $id;
+ }
+
+
+//getters
+ 
+ function getLabel() {
+  return $this->label;
+ }
+
+  function getId() {
+  return $this->id;
+ }
+ 
+ function getPays() {
+  return $this->pays;
+ }
+
+ //Functions
+ 
+ 
+// retourne une liste des id
+ public static function getAllId() {
+  try {
+   $database = Model::getInstance();
+   $query = "select id from producteur";
+   $statement = $database->prepare($query);
+   $statement->execute();
+   $results = $statement->fetchAll(PDO::FETCH_COLUMN, 0);
+   return $results;
+  } catch (PDOException $e) {
+   printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+   return NULL;
+  }
+ }
+
+ public static function getMany($query) {
+  try {
+   $database = Model::getInstance();
+   $statement = $database->prepare($query);
+   $statement->execute();
+   $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelVin");
+   return $results;
+  } catch (PDOException $e) {
+   printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+   return NULL;
+  }
+ }
+
+ public static function getAll() {
+  try {     
+   $database = Model::getInstance();
+   $query = "select * from banque";
+   $statement = $database->prepare($query);
+   $statement->execute();
+   $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelBanque");
+   return $results;
+  } catch (PDOException $e) {
+   printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+   return NULL;
+  }
+ }
+
+ public static function getOne($id) {
+  try {
+   $database = Model::getInstance();
+   $query = "select * from producteur where id = :id";
+   $statement = $database->prepare($query);
+   $statement->execute([
+     'id' => $id
+   ]);
+   $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelProducteur");
+   return $results;
+  } catch (PDOException $e) {
+   printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+   return NULL;
+  }
+ }
+
+ public static function insert($nom, $prenom, $region) {
+  try {
+   $database = Model::getInstance();
+
+   // recherche de la valeur de la clé = max(id) + 1
+   $query = "select max(id) from producteur";
+   $statement = $database->query($query);
+   $tuple = $statement->fetch();
+   $id = $tuple['0'];
+   $id++;
+
+   // ajout d'un nouveau tuple;
+   $query = "insert into producteur value (:id, :nom, :prenom, :region)";
+   $statement = $database->prepare($query);
+   $statement->execute([
+     'id' => $id,
+     'nom' => $nom,
+     'prenom' => $prenom,
+     'region' => $region
+   ]);
+   return $id;
+  } catch (PDOException $e) {
+   printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+   return -1;
+  }
+ }
+
+ public static function update() {
+  echo ("ModelVin : update() TODO ....");
+  return null;
+ }
+
+ public static function delete($id) {
+  try {
+   $database = Model::getInstance();
+   
+   //Savoir si le producteur est présent dans recolte
+   $query = "SELECT COUNT(*) FROM recolte WHERE producteur_id = :id";
+   $statement = $database->prepare($query);
+   $statement->execute([
+     'id' => $id
+   ]);
+   $results = $statement->fetchColumn();
+   
+   //On supprime le producteur ou non
+   if ($results == 0){
+       //Supprimer le producteur
+   $query = "DELETE FROM producteur where id = :id";
+   $statement = $database->prepare($query);
+   $statement->execute([
+     'id' => $id
+   ]);
+   return 1;
+   } 
+   else {
+       //On ne le supprime pas, il est présent dans une récolte
+       return 2;
+   }
+   
+  } catch (PDOException $e) {
+   printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+   return 0;
+  }
+ }
+
+  public static function getAllRegion() {
+  try {     
+   $database = Model::getInstance();
+   $query = "SELECT DISTINCT region FROM `producteur` ";
+   $statement = $database->prepare($query);
+   $statement->execute();
+   $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelProducteur");
+   return $results;
+  } catch (PDOException $e) {
+   printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+   return NULL;
+  }
+ }
+ 
+ public static function getAllRegionProd() {
+  try {     
+   $database = Model::getInstance();
+   $query = "SELECT region, COUNT(*) AS nombre_producteurs FROM producteur GROUP BY region;` ";
+   $statement = $database->prepare($query);
+   $statement->execute();
+   $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelProducteur");
+   return $results;
+  } catch (PDOException $e) {
+   printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+   return NULL;
+  }
+ }
+ 
+}
+?>
+<!-- ----- fin ModelVin -->
---
+++ app/view/banque/viewInsertedProd.php
@@ -0,0 +1,35 @@
+
+<!-- ----- début viewInserted -->
+<?php
+require ($root . '/app/view/fragment/fragmentCaveHeader.html');
+?>
+
+<body>
+  <div class="container">
+    <?php
+    include $root . '/app/view/fragment/fragmentCaveMenu.html';
+    include $root . '/app/view/fragment/fragmentCaveJumbotron.html';
+    ?>
+    <!-- ===================================================== -->
+    <?php
+    if ($results) {
+     echo ("<h3>Le nouveau producteur a été ajouté </h3>");
+     echo("<ul>");
+     echo ("<li>id = " . $results . "</li>");
+     echo ("<li>nom = " . $_GET['nom'] . "</li>");
+     echo ("<li>prenom = " . $_GET['prenom'] . "</li>");
+     echo ("<li>region = " . $_GET['region'] . "</li>");
+     echo("</ul>");
+    } else {
+     echo ("<h3>Problème d'insertion du Vin</h3>");
+     echo ("id = " . $_GET['nom']);
+    }
+
+    echo("</div>");
+    
+    include $root . '/app/view/fragment/fragmentCaveFooter.html';
+    ?>
+    <!-- ----- fin viewInserted -->    
+
+    
+    
