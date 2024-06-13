eaa319ad9b86e40f6dcbacb292b459b54feb78bf
Dzi4dk <louis.2lhomme@gmail.com>
2024-06-13 17:12:02 +0200

    idée pour amélioration (divers)

--- public/documentation/mesPropositions.php
+++ public/documentation/mesPropositions.php
@@ -11,11 +11,12 @@ require ($root . '/app/view/fragment/fragmentCaveHeader.html');
       include $root . '/app/view/fragment/fragmentCaveJumbotron.html';
       ?>
 
-      <h4>Une correction de bug</h4><br/>
+      <h4>Une amélioration</h4><br/>
       
-      Une correction de bugUne correction de bugUne correction de bug<br/>
-      Une correction de bugUne correction de bugUne correction de bug<br/>
-      Une correction de bugUne correction de bug<br/><br/>
+      Il serait intéressant de mieux protéger les mots de passe des utlisateurs.<br/>
+      En effet, tout administrateur peut avoir accès aux mots de passe de tout les utlisateurs.<br/>
+      Il faudrait donc hacher les mots de passe ou utiliser un autre procédé afin qu'ils ne<br/>
+      Ne soient pas disponibles en clair.<br/><br/>
       
       <h4>Une idée géniale</h4><br/>
       
