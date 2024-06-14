8f2a94cea3cc9d707c3b44839dfc868de1aaa19d
Thomas_chbn <147448902+Darkman34@users.noreply.github.com>
2024-06-14 12:05:26 +0200

    Liste clients et admin corrigée

--- app/view/personne/viewAllPersonne.php
+++ app/view/personne/viewAllPersonne.php
@@ -14,18 +14,18 @@ require ($root . '/app/view/fragment/fragmentCaveHeader.html');
     <table class = "table table-striped table-bordered">
       <thead>
         <tr>
-          <th scope = "col">id</th>
-          <th scope = "col">nom</th>
-          <th scope = "col">prenom</th>
-          <th scope = "col">login</th>
+          <th scope = "col">Nom</th>
+          <th scope = "col">Prénom</th>
+          <th scope = "col">Login</th>
+          <th scope = "col">Mot de passe</th>
         </tr>
       </thead>
       <tbody>
           <?php
           // La liste des vins est dans une variable $results             
           foreach ($results as $element) {
-           printf("<tr><td>%d</td><td>%s</td><td>%s</td><td>%s</td></tr>", $element->getId(), $element->getNom(), 
-             $element->getPrenom(), $element->getLogin());
+           printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>", $element->getNom(), 
+             $element->getPrenom(), $element->getLogin(), $element->getPassword());
           }
           ?>
       </tbody>
