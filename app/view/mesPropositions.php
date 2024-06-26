39fc29564e7557b79b5cf009d0e35ad5285f19d6
Thomas_chbn <147448902+Darkman34@users.noreply.github.com>
2024-06-26 13:21:18 +0200

    V2

--- app/view/mesPropositions.php
+++ app/view/mesPropositions.php
@@ -10,19 +10,22 @@ require ($root . '/app/view/fragment/fragmentCaveHeader.html');
       include $root . '/app/view/fragment/fragmentCaveJumbotron.html';
       ?>
 
-      <h4>Une amélioration</h4><br/>
+      <h4>Amélioration 1</h4><br/>
       
-      Il serait intéressant de mieux protéger les mots de passe des utlisateurs.<br/>
-      En effet, tout administrateur peut avoir accès aux mots de passe de tout les utlisateurs.<br/>
-      Il faudrait donc hacher les mots de passe ou utiliser un autre procédé afin qu'ils ne<br/>
-      Ne soient pas disponibles en clair.<br/><br/>
+      On ne pouvait pas guet une valeur au sein d'un même model ce qui nous a posé quelques problèmes même si on a pu les contourner<br/>
       
-      <h4>Une idée géniale</h4><br/>
+      <h4>Amélioration 2</h4><br/>
       
-      Une idée géniale<br/>
-      Une idée géniale<br/>
-      Une idée géniale<br/>
-      Une idée géniale<br/><br/>
+      Les mots de passe ne sont pas hashés ce qui augmentent très fortement les failles de sécurité. Cette amélioration ne concerne pas forcément notre modèle MVC mais plutôt notre projet en général.<br/><br/>
+      
+      <h4>Amélioration 3</h4><br/>
+      
+      Lors de l'achat des résidences par exemple on a du faire plusieurs aller retour entre les vues, les contrôleur et les modèles en donnant des input caché pour récupérer et garder les informations du premier formulaire ce qui fait perdre beaucoup de temps puisqu'on doit refaire plusieurs vues, plusieurs méthodes par contrôleur/modèles. On aurait pu utiliser des sessions pour stocker les variables mais on a pas trouvé utile de la faire dans ce cas la.<br/><br/>
+      
+      <h4>Amélioration 4</h4><br/>
+      
+      On aurait pu réutiliser des vues probablement. Dans notre projet c'était pas forcément très intéressant, mais pour des projets beaucoup plus gros, où on print plus de fois la même chose (une liste de comptes par exemple) il est intéressant d'utiliser des vues partielles (fragments) comme on a fait avec fragmentCaveFooter...<br/><br/>
+      
   </div>
   <?php include $root . '/app/view/fragment/fragmentCaveFooter.html'; ?>
 
